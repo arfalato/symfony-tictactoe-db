d1ce6d920c5894702a27a0d6c204c186e5c0ef43
Angelo Raffaele Falato <rfalato@gmail.com>
2022-08-09 12:01:45 +0200

    Update BoardService.php

--- src/Service/BoardService.php
+++ src/Service/BoardService.php
@@ -25,15 +25,20 @@ class BoardService
 	
     public function findAll()
     {
-	$find = $this->repo->findAll();
-	if (empty($find)) {
-	    $find['message'] = ['No board found'];
-	    $find['status'] = self::NOT_FOUND ;
+	    $find = $this->repo->findAll();
+		
+	    if (empty($find)) {
+			
+	        $find['message'] = ['No board found'];
+	        $find['status'] = self::NOT_FOUND ;
+			
+	        return $find;
+	    }
+		
+	    $find['message'] = $find;
+	    $find['status'] = self::OK;
+		
 	    return $find;
-	}
-	$find['message'] = $find;
-	$find['status'] = self::OK;
-	return $find;
     }
 	
     public function post() : array
